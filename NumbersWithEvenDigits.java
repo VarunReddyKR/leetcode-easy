fc4cd65a2afaeb3e16c7fe45a339aeae88158f6d
VarunReddy <123507581+VarunReddyKR@users.noreply.github.com>
2024-09-15 00:50:26 +0530

    Update NumbersWithEvenDigits.java

--- NumbersWithEvenDigits.java
+++ NumbersWithEvenDigits.java
@@ -1,30 +1,20 @@
-// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
-
 class Solution {
     public int countDigitsOfANumber(int num) {
-        int count = 0;
-        while (num > 0) {
-            count++;
-            num = num / 10;
+        if (num == 0) {
+            return 1; 
         }
-        return count;
-    }
-
-    public boolean isEven(int num) {
-        return num % 2 == 0;
+        return (int) Math.log10(num) + 1;
     }
 
     public int findNumbers(int[] nums) {
         int count = 0;
-        if (nums.length == 0) {
-            return -1;
-        }
 
-        for (int i = 0; i < nums.length; i++) {
-            if (isEven(countDigitsOfANumber(nums[i]))) {
+        for (int num : nums) {
+            if (countDigitsOfANumber(num) % 2 == 0) {
                 count++;
             }
         }
+
         return count;
     }
 }
